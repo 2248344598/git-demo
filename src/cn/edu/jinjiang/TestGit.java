6932fd8e53bf02d4c31ea08c22bc2856ff0ea5a9
panfeng <820017247@qq.com>
2022-11-03 14:15:33 +0800

    idea three master commit

--- src/cn/edu/jinjiang/TestGit.java
+++ src/cn/edu/jinjiang/TestGit.java
@@ -2,7 +2,6 @@ package cn.edu.jinjiang;
 
 public class TestGit {
     public static void main(String[] args) {
-        System.out.println("git test");
-        System.out.println("git test2222");
+        System.out.println("master");
     }
 }
