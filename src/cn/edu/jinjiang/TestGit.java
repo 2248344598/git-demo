8eef56affaceaf6754d82dd63baf8442d04961b7
panfeng <820017247@qq.com>
2022-11-03 14:05:20 +0800

    idea second commit

--- src/cn/edu/jinjiang/TestGit.java
+++ src/cn/edu/jinjiang/TestGit.java
@@ -3,5 +3,6 @@ package cn.edu.jinjiang;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("git test");
+        System.out.println("git test2222");
     }
 }
